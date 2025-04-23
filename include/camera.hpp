2cee6266abb9761b310420c3598484fcbae63027
jspast <140563347+jspast@users.noreply.github.com>
2025-04-23 00:15:38 -0300

    Initial template based on Lab 2

---
+++ include/camera.hpp
@@ -0,0 +1,160 @@
+#pragma once
+
+#include "matrices.hpp"
+
+#include <glm/mat4x4.hpp>
+#include <glm/vec4.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#define EPSILON (std::numeric_limits<float>::epsilon())
+#define SQRT_EPSILON (0.000345)
+
+template<typename Base, typename T>
+inline bool instanceof(const T *ptr);
+
+// Abstract class that defines a camera
+class Camera {
+    protected:
+        // The up vector is fixed to the global y
+        glm::vec4 up_vector = glm::vec4(0.0f,1.0f,0.0f,0.0f);
+
+        // Phi is limited to (-pi/2, +pi/2) to prevent camera flips
+        constexpr static const float phimax = M_PI_2f - 4 * SQRT_EPSILON;
+        constexpr static const float phimin = -phimax;
+
+        // The camera position
+        float x = 0.0f;
+        float y = 0.0f;
+        float z = 0.0f;
+
+        // The camera angles
+        float theta = 0.0f;
+        float phi   = 0.0f;
+
+        // Used to define the frustum
+        float nearplane_distance = -0.1f;
+        float farplane_distance  = -10.0f;
+
+        // Perspective projection is used by default
+        bool use_perspective_projection = true;
+
+        float fov          = M_PIf / 3.0f;
+        float aspect_ratio = 1.0f;
+
+        // Zoom is simulated in orthographic projection by changing the size of the projection plane
+        float orthographic_zoom = 1.5f;
+        float orthographic_zoom_min = 1.1f;
+        float orthographic_zoom_max = 10.0f;
+
+        // Function called after a theta or phi change
+        // Designed to be used when an update in other camera atributes is necessary
+        virtual void update() = 0;
+
+    public:
+        // Returns the unnormalized view vector
+        virtual glm::vec4 get_view_vector() = 0;
+
+        glm::mat4 get_view_matrix();
+        glm::mat4 get_projection_matrix();
+
+        void set_position(float x, float y, float z);
+        void set_position(glm::vec4 position);
+        glm::vec4 get_position();
+
+        // Adds theta_increment to theta and phi_increment to phi
+        // Should be used when both angles are updated at the same time to avoid unnecessary updates
+        void adjust_angles(float theta_increment, float phi_increment);
+
+        // Should be used when both angles are updated at the same time to avoid unnecessary updates
+        void set_angles(float theta, float phi);
+
+        // Adds increment to theta
+        void adjust_theta(float increment);
+
+        // Adds increment to phi
+        void adjust_phi(float increment);
+
+        void set_theta(float theta);
+        void set_phi(float phi);
+        float get_theta();
+        float get_phi();
+
+        void set_nearplane_distance(float distance);
+        void set_farplane_distance(float distance);
+        float get_nearplane_distance();
+        float get_farplane_distance();
+
+        // When boolean is true, perspective projection is used
+        // When boolean is false, orthographic projection is used
+        void toggle_perspective_projection(bool boolean);
+
+        bool is_projection_perspective();
+
+        void set_fov(float fov);
+        float get_fov();
+
+        void set_aspect_ratio(float aspect_ratio);
+        float get_aspect_ratio();
+
+        // Adds increment to orthographic zoom
+        void adjust_orthographic_zoom(float increment);
+
+        void set_orthographic_zoom(float zoom);
+        float get_orthographic_zoom();
+
+        void set_orthographic_zoom_min(float min);
+        float get_orthographic_zoom_min();
+
+        void set_orthographic_zoom_max(float max);
+        float get_orthographic_zoom_max();
+};
+
+// A Look-At implementation of a camera
+class LookAtCamera: public Camera {
+    protected:
+        // The camera distance relative to the target position
+        // Should never be 0, which would cause division by zero problems
+        float distance = 3.0f;
+
+        // The target point position
+        float target_x = 1.0f;
+        float target_y = 0.0f;
+        float target_z = 0.0f;
+
+        void update();
+
+    public:
+        void set_target_position(float x, float y, float z);
+        void set_target_position(glm::vec4 position);
+        glm::vec4 get_target_position();
+
+        glm::vec4 get_view_vector();
+
+        // Adds increment to distance
+        void adjust_distance(float increment);
+
+        void set_distance(float distance);
+        float get_distance();
+};
+
+// An implementation of a free camera
+class FreeCamera: public Camera {
+    protected:
+        // Vectors used to define the camera direction
+        glm::vec4 w_vector = glm::vec4(1.0f, 0.0f, 0.0f, 0.0f);
+        glm::vec4 u_vector = crossproduct(up_vector, w_vector) / norm(crossproduct(up_vector, w_vector));
+        glm::vec4 v_vector = crossproduct(w_vector, u_vector);
+
+        void update();
+
+    public:
+        glm::vec4 get_view_vector();
+
+        // Moves the camera relative to the view_vector
+        void move(float forward_displacement, float left_displacement);
+};
+
+FreeCamera build_free_camera(Camera *camera);
+
+LookAtCamera build_lookat_camera(Camera *camera, float distance = 1.0f);
+LookAtCamera build_lookat_camera(Camera *camera, glm::vec4 target_position, float distance);
---
+++ src/camera.cpp
@@ -0,0 +1,320 @@
+#include "camera.hpp"
+
+#include "glm/common.hpp"
+#include "glm/ext/vector_float4.hpp"
+#include <cmath>
+
+template<typename Base, typename T>
+inline bool instanceof(const T *ptr) {
+    return dynamic_cast<const Base*>(ptr) != nullptr;
+}
+
+void Camera::set_position(float nx, float ny, float nz)
+{
+    x = nx;
+    y = ny;
+    z = nz;
+}
+
+void Camera::set_position(glm::vec4 p)
+{
+    x = p.x;
+    y = p.y;
+    z = p.z;
+}
+
+glm::vec4 Camera::get_position()
+{
+    return glm::vec4(x, y, z, 1.0f);
+}
+
+glm::mat4 Camera::get_view_matrix()
+{
+    return Matrix_Camera_View(get_position(), get_view_vector(), up_vector);
+}
+
+glm::mat4 Camera::get_projection_matrix()
+{
+    glm::mat4 projection;
+
+    if (use_perspective_projection)
+    {
+        projection = Matrix_Perspective(fov, aspect_ratio, nearplane_distance, farplane_distance);
+    }
+    else
+    {
+        // Simulates zoom by changing the size of the projection plane
+        float t = 1.5f / log2(orthographic_zoom);
+        float b = -t;
+        float r = t * aspect_ratio;
+        float l = -r;
+        projection = Matrix_Orthographic(l, r, b, t, nearplane_distance, farplane_distance);
+    }
+
+    return projection;
+}
+
+void Camera::adjust_angles(float ti, float pi)
+{
+    theta += ti;
+    phi = glm::clamp(phi + pi, phimin, phimax);
+    update();
+}
+
+void Camera::set_angles(float t, float p)
+{
+    theta = t;
+    phi   = glm::clamp(p, phimin, phimax);
+    update();
+}
+
+void Camera::adjust_theta(float increment)
+{
+    theta += increment;
+    update();
+}
+
+void Camera::set_theta(float t)
+{
+    theta = t;
+    update();
+}
+
+void Camera::adjust_phi(float increment)
+{
+    phi = glm::clamp(phi + increment, phimin, phimax);
+    update();
+}
+
+void Camera::set_phi(float p)
+{
+    phi = glm::clamp(p, phimin, phimax);
+    update();
+}
+
+float Camera::get_theta(){
+    return theta;
+}
+
+float Camera::get_phi()
+{
+    return phi;
+}
+
+void Camera::set_nearplane_distance(float d)
+{
+    nearplane_distance = d;
+}
+
+void Camera::set_farplane_distance(float d)
+{
+    farplane_distance = d;
+}
+
+float Camera::get_nearplane_distance()
+{
+    return nearplane_distance;
+}
+
+float Camera::get_farplane_distance()
+{
+    return farplane_distance;
+}
+
+void Camera::toggle_perspective_projection(bool b)
+{
+    use_perspective_projection = b;
+}
+
+bool Camera::is_projection_perspective()
+{
+    return use_perspective_projection;
+}
+
+void Camera::set_fov(float f)
+{
+    fov = f;
+}
+
+float Camera::get_fov()
+{
+    return fov;
+}
+
+void Camera::set_aspect_ratio(float a)
+{
+    aspect_ratio = a;
+}
+
+float Camera::get_aspect_ratio()
+{
+    return aspect_ratio;
+}
+
+void Camera::adjust_orthographic_zoom(float z)
+{
+    orthographic_zoom = glm::clamp(orthographic_zoom + z, orthographic_zoom_min, orthographic_zoom_max);
+}
+
+void Camera::set_orthographic_zoom(float z)
+{
+    orthographic_zoom = glm::clamp(z, orthographic_zoom_min, orthographic_zoom_max);
+}
+
+float Camera::get_orthographic_zoom()
+{
+    return orthographic_zoom;
+}
+
+void Camera::set_orthographic_zoom_min(float m)
+{
+    orthographic_zoom_min = m;
+}
+
+float Camera::get_orthographic_zoom_min()
+{
+    return orthographic_zoom_min;
+}
+
+void Camera::set_orthographic_zoom_max(float m)
+{
+    orthographic_zoom_max = m;
+}
+
+float Camera::get_orthographic_zoom_max()
+{
+    return orthographic_zoom_max;
+}
+
+// Update camera position based on the camera angles
+void LookAtCamera::update()
+{
+    y = distance * sinf(phi) + target_y;
+    z = distance * cosf(phi) * cosf(theta) + target_z;
+    x = distance * cosf(phi) * sinf(theta) + target_x;
+}
+
+void LookAtCamera::set_target_position(float nx, float ny, float nz)
+{
+    target_x = nx;
+    target_y = ny;
+    target_z = nz;
+}
+
+void LookAtCamera::set_target_position(glm::vec4 p)
+{
+    target_x = p.x;
+    target_y = p.y;
+    target_z = p.z;
+}
+
+glm::vec4 LookAtCamera::get_target_position()
+{
+    return glm::vec4(target_x, target_y, target_z, 1.0f);
+}
+
+// Computes the view vector based on the camera and target positions
+glm::vec4 LookAtCamera::get_view_vector()
+{
+    return get_target_position() - get_position();
+}
+
+void LookAtCamera::adjust_distance(float increment)
+{
+    // Avoids target and camera positions being the same, which would cause division by zero problems
+    distance = fmax(SQRT_EPSILON, distance + increment);
+    update();
+}
+
+void LookAtCamera::set_distance(float d)
+{
+    // Avoids target and camera positions being the same, which would cause division by zero problems
+    distance = fmax(SQRT_EPSILON, d);
+    update();
+}
+
+float LookAtCamera::get_distance()
+{
+    return distance;
+}
+
+// Update camera vectors based on the camera angles
+void FreeCamera::update()
+{
+    w_vector = get_view_vector() / norm(get_view_vector());
+    u_vector = crossproduct(up_vector, w_vector) / norm(crossproduct(up_vector, w_vector));
+    v_vector = crossproduct(w_vector, u_vector);
+}
+
+// Computes the view vector based on the camera angles
+glm::vec4 FreeCamera::get_view_vector()
+{
+    return glm::vec4(cos(phi) * sin(theta), -sin(phi), cos(phi) * cos(theta), 0.0f);
+}
+
+void FreeCamera::move(float forward_displacement, float left_displacement)
+{
+    set_position(get_position() + w_vector * forward_displacement + u_vector * left_displacement);
+}
+
+FreeCamera build_free_camera(Camera* camera)
+{
+    // Do nothing if camera is already a FreeCamera
+    if (instanceof<FreeCamera>(camera))
+        return *static_cast<FreeCamera*>(camera);
+
+    FreeCamera fc;
+
+    // Update camera angles
+    fc.set_angles(camera->get_theta() + M_PIf, camera->get_phi());
+
+    // Copy other camera properties
+    fc.set_position(camera->get_position());
+    fc.set_nearplane_distance(camera->get_nearplane_distance());
+    fc.set_farplane_distance(camera->get_farplane_distance());
+    fc.toggle_perspective_projection(camera->is_projection_perspective());
+    fc.set_aspect_ratio(camera->get_aspect_ratio());
+    fc.set_fov(camera->get_fov());
+    fc.set_orthographic_zoom(camera->get_orthographic_zoom());
+    fc.set_orthographic_zoom_min(camera->get_orthographic_zoom_min());
+    fc.set_orthographic_zoom_max(camera->get_orthographic_zoom_max());
+
+    return fc;
+}
+
+LookAtCamera build_lookat_camera(Camera *camera, float distance)
+{
+    // Compute the target position keeping camera position and orientation based on the desired distance
+    glm::vec4 target_position = camera->get_position() + distance * camera->get_view_vector() / norm(camera->get_view_vector());
+
+    return build_lookat_camera(camera, target_position, distance);
+}
+
+LookAtCamera build_lookat_camera(Camera *camera, glm::vec4 target_position, float distance)
+{
+    // Do nothing if camera is already a LookAtCamera
+    if (instanceof<LookAtCamera>(camera))
+        return *static_cast<LookAtCamera*>(camera);
+
+    LookAtCamera lc;
+
+    // Set new camera properties
+    lc.set_target_position(target_position);
+    lc.set_distance(distance);
+
+    // Update camera angles
+    lc.set_angles(camera->get_theta() + M_PIf, camera->get_phi());
+
+    // Copy other camera properties
+    lc.set_position(camera->get_position());
+    lc.set_nearplane_distance(camera->get_nearplane_distance());
+    lc.set_farplane_distance(camera->get_farplane_distance());
+    lc.toggle_perspective_projection(camera->is_projection_perspective());
+    lc.set_aspect_ratio(camera->get_aspect_ratio());
+    lc.set_fov(camera->get_fov());
+    lc.set_orthographic_zoom(camera->get_orthographic_zoom());
+    lc.set_orthographic_zoom_min(camera->get_orthographic_zoom_min());
+    lc.set_orthographic_zoom_max(camera->get_orthographic_zoom_max());
+
+    return lc;
+}
